25d554ead84c5fed31a62b0682787cc2fa91de5b
Marcus Vinícius <marcusvinicius@bizubox.com>
2018-05-31 20:10:23 -0300

    Shopping cycle [N customers x 1 worker] done using semaphore.

--- src/main.c
+++ src/main.c
@@ -2,18 +2,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 #define CUSTOMERS_NUMBER 10
 
 void *customer_go_shopping_in_the_market(void *arg);
+void *worker_verify_customer_shopping(void *arg);
 
-pthread_t customers[CUSTOMERS_NUMBER];
+sem_t sem_market_cashier;
+sem_t sem_customer_on_cashier;
+sem_t sem_customer_served;
 
 int main() {
+  pthread_t customers[CUSTOMERS_NUMBER];
+  pthread_t worker;
+  int customers_id[CUSTOMERS_NUMBER];
+
+  sem_init(&sem_market_cashier, 0, 1);
+  sem_init(&sem_customer_on_cashier, 0, 0);
+  sem_init(&sem_customer_served, 0, 0);
+
+  pthread_create(&worker, NULL, worker_verify_customer_shopping, NULL);
+
   for (int i = 0; i < CUSTOMERS_NUMBER; i++) {
-    int *id = (int *) malloc(sizeof (int));
-    *id = i;
-    pthread_create(customers, NULL, customer_go_shopping_in_the_market, (void *) (id));
+    customers_id[i] = i;
+    pthread_create(&customers[i], NULL, customer_go_shopping_in_the_market, (void *) &customers_id[i]);
   }
 
   for (int i = 0; i < CUSTOMERS_NUMBER; i++) {
@@ -26,7 +39,28 @@ int main() {
 void *customer_go_shopping_in_the_market(void *arg) {
   int id = *((int *) arg);
 
-  printf("Customer[%d] enters in supermarket.\n", id);
+  printf("Customer~%d enters in supermarket.\n", id);
+  sleep(1);
+  printf("Customer~%d picks some items and goes to supermarket checkout to pay its shopping.\n", id);
+  sleep(1);
 
+  sem_wait(&sem_market_cashier);
+  sem_post(&sem_customer_on_cashier);
+  printf("\nCustomer~%d is paying its shopping.\n", id);
+  sem_wait(&sem_customer_served);
+  printf("Customer~%d leaves the market.\n\n", id);
+  sleep(1);
+  sem_post(&sem_market_cashier);
+
+  pthread_exit(0);
+}
+
+void *worker_verify_customer_shopping(void *arg) {
+  while (1) {
+    sem_wait(&sem_customer_on_cashier);
+    printf("Worker checks customer's shopping and emits sales receipts.\n");
+    sleep(1);
+    sem_post(&sem_customer_served);
+  }
   pthread_exit(0);
 }
